198251bf36a51a9e9497b68e8c18a199b08fc5d0
Matheus Ferreira <matheusfer0902@gmail.com>
2024-09-05 18:10:16 -0300

    Finalização das rotas delete e put e integração com o banco de dados mongoDB usando prisma

--- server.js
+++ server.js
@@ -1,20 +1,68 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
+app.post('/users', async (req, res) => {
 
-app.post('/users', (req, res) => {
+    await prisma.user.create({
+        data: {
+          name: req.body.name,
+          email: req.body.email,
+          age: req.body.age
+        }
+    })
 
-    users.push(req.body)
-
-    res.send('Ok post')
+    res.status(203).send('Ok post')
 })
 
-app.get('/users', (req, res) => {
+app.delete('/users/:id', async (req, res) => {
 
-    res.json(users)
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(202).send('Ok delete')
+})
+
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).send('Ok put')
+    
+})
+
+app.get('/users', async (req, res) => {
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: parseInt(req.query.age),
+                email: req.query.email
+            }
+        })
+    } else {
+        users = await prisma.user.findMany()
+    }
+
+    res.status(200).json(users)
 
 })
 
